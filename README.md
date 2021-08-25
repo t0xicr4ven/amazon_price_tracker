c0184dc1527f842a1dc58d957602e85820ce8977
t0xicr4ven <57691355+t0xicr4ven@users.noreply.github.com>
2021-08-25 17:47:03 +1000

    FINISHED - will send email if price is below max price willing to pay

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Amazon Price Tracker
+
+Set url of item you want to watch and set the max price you are willing to pay.
+
+If price is below the max price it will send you an email saying so.
+
+#### Packages
+-
+* Beautiful Soup
+* smtplib
+* os
+* lxml
+* requests
+
+-
+
+__Only difficult step is setting up your email, to allow access from the program__
--- main.py
+++ main.py
@@ -1,18 +1,47 @@
 import requests
 from bs4 import BeautifulSoup 
 import lxml
+import smtplib
+import os
 
-WEB_ADDRESS = "https://www.amazon.com.au/Samsung-Inch-Monitor-S27F350FHE-LS27R350FHEXXY/dp/B085D9DTJN/ref=sr_1_20?crid=18GRSO18K97ZL&dchild=1&keywords=widescreen+computer+monitor&qid=1629783463&sprefix=widescreen+%2Caps%2C342&sr=8-20"
+
+"""You could put a bunch of items into a csv and loop through and check price
+on each"""
+
+EMAIL_ADD = os.environ.get('email_add')
+EMAIL_PASS = os.environ.get('email_pass')
+
+MAX_PRICE_TO_PAY = 1800
 IPAD_ADDRESS = "https://www.amazon.com.au/Apple-12-9-inch-iPad-Pro-Wi-Fi-256GB/dp/B093R19M8X/ref=sr_1_5?crid=2E1MDCDLO1YBB&dchild=1&keywords=ipad+pro+12.9&qid=1629786015&sprefix=ipad+%2Caps%2C347&sr=8-5"
-HEADER = {
-        'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
-        'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8'
-        }
+
+ HEADER = {
+         'User-Agent': """SET YOUR USER AGENT. GOOGLE MY USER AGENT AND YOU'LL
+         FIND IT""",
+          'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8'
+       }
+
 response = requests.get(url=IPAD_ADDRESS, headers=HEADER)
 soup = BeautifulSoup(response.text, 'lxml')
+
 # get price of item and remove the $ 
 price_string = soup.find('span',
         id='priceblock_ourprice').getText().strip('$')
+
 # remove , if string has one
 new_string = price_string.replace(',','') 
-print(int(float(new_string)))
+
+# check if price is below users set price
+def check_if_cheap(current_price):
+    if current_price < MAX_PRICE_TO_PAY:
+        send_email()
+    else:
+        pass
+
+# send email sayin price is below max price willing to pay and add the link
+def send_email():
+    with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+        connection.starttls()
+        connection.login(user=EMAIL_ADD, password=EMAIL_PASS)
+        connection.sendmail(from_addr=EMAIL_ADD,to_addrs=EMAIL_ADD,msg=f"""Subject:
+                Amazon Item\n\nThe it below  your price, go buy NOW --
+                {IPAD_ADDRESS}""")
